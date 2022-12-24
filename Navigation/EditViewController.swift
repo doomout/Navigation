44bf5ed5530d26a8267a16fadc5280f434218db6
doomout <winquake3@hanmail.net>
2022-12-24 16:49:50 +0900

    메인 , 수정 화면 간에 메세지 전달 구현

--- Navigation/EditViewController.swift
+++ Navigation/EditViewController.swift
@@ -1,20 +1,31 @@
 import UIKit
 
+//프로토콜이란? 특정 객체가 갖춰야할 기능이나 속성에 대한 설계도, c#의 델리게이트와 같다.
+protocol EditDelegate {
+    func didMessageEditDone(_ controller: EditViewController, message:String)
+}
+
 class EditViewController: UIViewController {
     var textWayValue: String = ""
+    var textMessage: String = ""
+    var delegate: EditDelegate?
     
     @IBOutlet var lblWay: UILabel!
-    
+    @IBOutlet var txMessage: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         lblWay.text = textWayValue
+        txMessage.text = textMessage
     }
     
 
     @IBAction func btnDona(_ sender: UIButton) {
+        if delegate != nil {
+            delegate?.didMessageEditDone(self, message: txMessage.text!)
+        }
         //세그웨이 추가할 때 Show 형태로 했기에 되돌아 갈 땐 pop 형태로 해야 한다.
         _ = navigationController?.popViewController(animated: true)
     }
--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -1,8 +1,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, EditDelegate {
+   
+    
 
+    @IBOutlet var txMessage: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,6 +21,12 @@ class ViewController: UIViewController {
         } else if segue.identifier == "editBarButton" { //바 버튼을 클릭한 경우
             editViewController.textWayValue = "segue : use Bar button"
         }
+        editViewController.textMessage = txMessage.text!
+        editViewController.delegate = self
+    }
+    
+    func didMessageEditDone(_ controller: EditViewController, message: String) {
+        txMessage.text = message
     }
 }
 
